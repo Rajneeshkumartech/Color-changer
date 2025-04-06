9b63c79d068765922e21d8a3836e05d21bafaae6
Rajneeshkumartech <rajneeshkumar.tech@gmail.com>
2025-04-07 01:30:10 +0530

    Update color.js
    remove color alert from starting box

--- color.js
+++ color.js
@@ -3,16 +3,13 @@ buttons.forEach(function (box) {
     box.addEventListener('click',function (e){
         if (e.target.id == "grey"){
             document.body.style.background = e.target.id;
-            confirm(`are you sure can we change background color to ${e.target.id} `)
-            alert(`"background color is changed to" ${e.target.id}` )
+        
         } else if (e.target.id == "yellow"){
             document.body.style.background = e.target.id;
-             confirm(`are you sure can we change background color to ${e.target.id} `)
-            alert(`"background color is changed to" ${e.target.id}` )
+            
         } else if (e.target.id == "blue"){
             document.body.style.background = e.target.id
-             confirm(`are you sure can we change background color to ${e.target.id} `)
-            alert(`"background color is changed to" ${e.target.id}` )
+          
         } else if (e.target.id == "white"){
             document.body.style.background = e.target.id
              confirm(`are you sure can we change background color to ${e.target.id} `)
